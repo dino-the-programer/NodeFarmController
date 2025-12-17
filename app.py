e811550eaa2c87d89f014ad931f2c06052ddd2f5
Adrij <adrijpaul2004@gmail.com>
2025-12-17 22:38:06 +0530

    added the static html folder and mounted the dir

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from Controller.core.config import config
 from Controller.core.logging import setup_logging
 from Controller.db.schema import Base, engine
@@ -11,6 +12,7 @@ setup_logging()
 Base.metadata.create_all(bind=engine)
 tunnelUrl = tunnel.Tunnel.CreateTunnel(os.getenv("PINGGY_TOKEN"))
 publisher.publish(os.getenv("GITHUB_TOKEN"),publisher.DomainEndPoint(url=tunnelUrl,active=True))
-app = FastAPI(title=config.app_name)
 
+app = FastAPI(title=config.app_name)
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
