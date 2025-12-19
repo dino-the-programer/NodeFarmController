70bc9f5fd620a9f9472b64270fe685387a2c054f
Adrij <adrijpaul2004@gmail.com>
2025-12-19 13:39:59 +0530

    made the connection manager to only accept one connection per email

--- api/websocket/ConnectionManager.py
+++ api/websocket/ConnectionManager.py
@@ -1,5 +1,5 @@
 from typing import List, Union
-from fastapi import WebSocket
+from fastapi import WebSocket,status
 from Controller.models.message import SubcribeWorker
 from Controller.db.schema import SessionLocal
 from Controller.services.worker_service import WorkerService
@@ -11,22 +11,33 @@ def getWorkerService() -> WorkerService:
 class ConnectionManager:
     def __init__(self):
         self.active_connections: List[Connection] = []
+    
+    def search(self,param:Union[WebSocket,int])->Connection|None:
+        for connection in self.active_connections:
+            if connection.socket == param or connection.id == param:
+                return connection
 
     async def connect(self, websocket: WebSocket, clientInfo:SubcribeWorker):
-        await websocket.accept()
         service = getWorkerService()
         id = service.create_worker(clientInfo.email).id
+        if self.search(id) != None:
+            await websocket.accept()
+            await websocket.close(code=status.WS_1008_POLICY_VIOLATION,reason="Already Connected from another isntance")
+            return
         self.active_connections.append(Connection(socket=websocket,id=id))
+        await websocket.accept()
 
     def disconnect(self, connectionParam: Union[WebSocket,int]):
         for connection in self.active_connections:
             if connection.socket == connectionParam or connection.id == connectionParam:
                 self.active_connections.remove(connection)
     
-    async def send(self,connectionParam: Union[WebSocket,int],message:str):
+    async def send(self,connectionParam: Union[WebSocket,int],message:str) -> bool:
         for connection in self.active_connections:
             if connection.socket == connectionParam or connection.id == connectionParam:
                 await connection.socket.send_text(message)
+                return True
+        return False
 
     async def broadcast(self, message: str):
         for connection in self.active_connections:
